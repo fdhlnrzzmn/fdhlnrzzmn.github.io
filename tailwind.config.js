898be92d9d59c314a487cf771facdfe30d90c0f9
fdhlnrzzmn <fdhlnrzzmn@gmail.com>
2022-04-13 21:04:40 +0700

    "tailwind version"

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./public/**/*.{html, js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors:{
+        fdl : '#eeede9',
+        fdl2 : '#484848'
+      },
+      fontFamily :{
+        bebas:['Bebas Neue, cursive'],
+        montserrat:['Montserrat']
+      },
+      leading:'62px'
+    },
+  },
+  plugins: [],
+}
